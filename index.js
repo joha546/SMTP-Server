9319ac5502535f63cce41278af82ec7ed8a54db2
Md Khaled Bin Joha <joh60483@gmail.com>
2024-12-31 19:32:41 +0600

    aws payment method issue

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ const SMTPServer = require("smtp-server").SMTPServer;
 
 // SMTP Server instance.
 const server = new SMTPServer({
+    allowInsecureAuth: true, 
+    authOptional: true,
+    onConnect(session, cb){
+        // Debugging
+        console.log('onConnect', session.id)
+        cb()    // Accept connection.
+    },
 
+    onMailFrom(address, session, cb){
+        console.log('onMailFrom', address.address, session.id)
+        cb()
+    },
+
+    onRcptTo(address, session, cb){
+        console.log('onRcptTo', address.address, session.id)
+        cb()
+    },
+
+    onData(stream, session, cb){
+        stream.on('data', (data) => console.log('onData ${data.toString()}'))
+        stream.on('end', cb)
+    }
 });
 
 server.listen(25, ()=> console.log('Server Running on 25 port.'))
